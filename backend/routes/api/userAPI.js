4089cdf73acbb1916e223c2f28d6f06c56854ede
Amro Rabea <amroalsafy@gmail.com>
2025-04-19 23:17:19 +0200

    update user

--- backend/routes/api/userAPI.js
+++ backend/routes/api/userAPI.js
@@ -3,10 +3,29 @@ const router = express.Router();
 const userController = require('../../controllers/userController');
 const verifyJWT = require("../../middleware/verifyJWT");
 
-router.post('/register', verifyJWT.verifyJWT, userController.registerUser);
-router.put('/update/:id', userController.updateUser);
-router.get('/all', userController.getAllUsers);
-router.get('/user/:id', userController.getUser);
-router.delete('/delete/:id', userController.deleteUser);
+// @desc    Register a user
+// @route   POST /api/users/register
+// @access  Public (or you can set it to Private if authentication is needed)
+router.post('/register', userController.registerUser);
+
+// @desc    Update a user
+// @route   PUT /api/users/update/:id
+// @access  Private (authentication required)
+router.put('/update/:id', verifyJWT.verifyJWT, userController.updateUser);
+
+// @desc    Get all users
+// @route   GET /api/users/all
+// @access  Private (authentication required)
+router.get('/all', verifyJWT.verifyJWT, userController.getAllUsers);
+
+// @desc    Get a single user
+// @route   GET /api/users/user/:id
+// @access  Private (authentication required)
+router.get('/user/:id', verifyJWT.verifyJWT, userController.getUser);
+
+// @desc    Delete a user
+// @route   DELETE /api/users/delete/:id
+// @access  Private (authentication required)
+router.delete('/delete/:id', verifyJWT.verifyJWT, userController.deleteUser);
 
 module.exports = router;
